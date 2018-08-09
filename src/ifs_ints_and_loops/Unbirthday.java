2deea1c40eb9701e11b7a9aafb487ffd7a91131f
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-08 18:26:23 -0700

    Unbirthday ip

---
+++ src/ifs_ints_and_loops/Unbirthday.java
@@ -0,0 +1,9 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, message);
+	}
+}
